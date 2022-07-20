103a9994ab1a9b4a7a8e012b2b65e435572976fc
Алексей Панфилов <ap83@list.ru>
2022-07-20 13:22:47 +0300

    city added

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
         int a = 22;
         System.out.println("Hello " + a);
         System.out.println(a);
+        String city = "Moscow";
+        System.out.println(city + " " + a);
     }
 }
